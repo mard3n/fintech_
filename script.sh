f3bd0f9ea8a369e40e8e34c83352695d30051f43
mard3n <57296558+mard3n@users.noreply.github.com>
2021-03-03 20:45:31 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@
 for i in `docker container ls -aq`
 do
         name=`docker inspect $i --format '{{.Name}}'`
-        state=`docker inspect $i --format '{{.State.Status}}'`
+        status=`docker inspect $i --format '{{.State.Status}}'`
         ip=`docker inspect $i --format '{{.NetworkSettings.IPAddress}}'`
-        printf "%-20s | %5s | %9s \n" "$name" "$state" "$ip" >> status.txt
+        printf "%-20s | %5s | %9s \n" "$name" "$status" "$ip" >> status.txt
 done
